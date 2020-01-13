5e6fcc46160c73f87afbef087788aa517c92b32f
xiaotian <xiaotian9202@gmail.com>
2020-01-13 21:13:40 +0800

    [yanxiaotian] {fix}-{collection-calculte-camp-es6}: modify code use es6

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,9 +1,10 @@
 'use strict';
-function one_add_next_multiply_three(collection){
-  var result = [];
-  for (var i = 0, lens = collection.length; i < lens - 1; i++) {
-    result.push((collection[i] + collection[i+1]) * 3);
-  }
-  return result;
+
+function one_add_next_multiply_three(collection) {
+    let result = [];
+    for (let i = 0, lens = collection.length; i < lens - 1; i++) {
+        result.push((collection[i] + collection[i + 1]) * 3);
+    }
+    return result;
 }
 module.exports = one_add_next_multiply_three;
--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,17 +1,14 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
-  collection = collection.sort(compare);
-  for (var i = 0, lens = collection.length; i + 3 < lens; i += 3) {
-    var temp = collection[i];
-    collection[i] = collection[i+1];
-    collection[i+1] = collection[i+2];
-    collection[i+2] = temp;
-  }
-  return collection;
-}
 
-function compare(a, b) {
-  return a - b;
+function rank_by_two_large_one_small(collection) {
+    collection = collection.sort((a, b) => a - b);
+    for (let i = 0, lens = collection.length; i + 3 < lens; i += 3) {
+        let temp = collection[i];
+        collection[i] = collection[i + 1];
+        collection[i + 1] = collection[i + 2];
+        collection[i + 2] = temp;
+    }
+    return collection;
 }
 
 module.exports = rank_by_two_large_one_small;
